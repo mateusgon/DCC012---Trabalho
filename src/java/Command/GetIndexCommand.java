35071c31c96780dfdce5138553876f3b88beee0e
Mateus Gonçalo <mateusgon57@gmail.com>
2018-08-15 14:47:52 -0300

    Iniciando com o front-end

---
+++ src/java/Command/GetIndexCommand.java
@@ -0,0 +1,17 @@
+package Command;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetIndexCommand implements Comando{
+
+    @Override
+    public void exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispacher = request.getRequestDispatcher("/WEB-INF/index.jsp");
+        dispacher.forward(request, response);
+    }
+    
+}
---
+++ web/WEB-INF/index.jsp
@@ -0,0 +1,6 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@include file="jspf/cabecalho.jspf" %>
+    <div class="container text-center">
+        <a href="ler.html"> <button type="button" class="btn btn-success">Success</button> </a>
+    </div>
+ <%@include file="jspf/rodape.jspf" %>
